d2d373a4a302e8829f8220406bd4f34b50fad286
zodd18 <josiesala@gmail.com>
2021-11-02 12:58:38 +0100

    depth 1 for rice submodules

--- setup-scripts/download-rice.sh
+++ setup-scripts/download-rice.sh
@@ -4,5 +4,5 @@ theme_name=$1
 notify-send -i ./setup-scripts/resources/white-brush.png "[INFO]: Downloading... \"$theme_name\" rice..." &
 success=0
 
-[ ! -z $theme_name ] && cd ./rices/$theme_name && git submodule update --init --depth 1.
+[ ! -z $theme_name ] && cd ./rices/$theme_name && git submodule update --init --depth 1
 notify-send -i ./setup-scripts/resources/white-brush.png "[INFO]: Download for \"$theme_name\" finished" &
